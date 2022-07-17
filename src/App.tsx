2f71d5203fb8e5a5756524008756f2da384b3c97
bekadan <bekadan@yahoo.com>
2022-07-17 12:38:29 +0300

    grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import React from "react";
+import SiteFooter from "./components/footer/SiteFooter";
 import SiteNavbar from "./components/navbar/SiteNavbar";
 
 function App() {
-  return <SiteNavbar />;
+  return (
+    <div className="site-main-wrapper">
+      <div className="site-navbar">
+        <SiteNavbar />
+      </div>
+      <div className="site-main">Main</div>
+      <div className="site-footer">
+        <SiteFooter />
+      </div>
+    </div>
+  );
 }
 
 export default App;
